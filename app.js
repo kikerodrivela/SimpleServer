2dff96e666dd8cbedaa35bc568361a940e242444
kikerodrivela <kikerodrivela@gmail.com>
2022-06-09 10:59:19 +0200

    added new features

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+var fs = require('fs');
+
 
 app.get("/", (request, response) => {
     response.send("Hi there");
@@ -13,10 +15,28 @@ app.get("/about", (request, response) => {
 app.use(express.json());
 
 app.post('/', function(request, response){
-    console.log(request.body);      // your JSON
-    response.send(request.body);    // echo the result back
+
+    console.log(".............................................");
+    console.log("Recieved a post request");
+    console.log("request.body: ", request.body);        // your JSON
+    response.send(request.body);                        // echo the result back
+    console.log(".............................................");
 });
 
+app.post('/sensores', function(request, response){
+    // Save to the request body to a file.json
+
+    console.log(request.body);      // your JSON
+    response.send(request.body);    // echo the result back
+
+    // Save to the request body to a file.json
+    // fs.writeFile('file.json', JSON.stringify(request.body), function (err) {
+    //     if (err) throw err;
+    //     console.log('Saved to file.json');
+    // });
+
+
+});
 
 
 
