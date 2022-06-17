9f93d519f512fb94b4d3d14e47b844c80189ef0e
kikerodrivela <kikerodrivela@gmail.com>
2022-06-17 15:15:04 +0200

    Update README and app.js

--- app.js
+++ app.js
@@ -7,15 +7,14 @@ app.get("/", (request, response) => {
     response.send("Hi there");
 });
 
-app.get("/about", (request, response) => {
-    response.send("<h1>About</h1>");
-});
-
-
 app.use(express.json());
 
-app.post('/', function(request, response){
+app.post('/', function (request, response) {
+    console.log(request.body);      // your JSON
+    response.send(request.body);    // echo the result back
+});
 
+app.post('/sensores', function (request, response) {
     date = new Date();
     datelocal = date.toLocaleString();
 
@@ -23,14 +22,9 @@ app.post('/', function(request, response){
     console.log("Recieved a post request");
     console.log("request.body: ", request.body);        // your JSON
 
-    console.log("Hora y fecha: ", datelocal);
     response.send(request.body);                        // echo the result back
     console.log(".............................................");
-});
 
-app.post('/sensores', function(request, response){
-    console.log(request.body);      // your JSON
-    response.send(request.body);    // echo the result back
 });
 
 
